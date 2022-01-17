56f8a2f1923d09ddb54821af518f5d7a9e2604ce
sahatnathanael <97328940+sahatnathanael@users.noreply.github.com>
2022-01-17 10:34:09 +0700

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,20 @@
+#fruits = ["Mangga", "Apel", "Durian"]
+#for fruit in fruits :
+#    print(fruit)
+
+#numbers = [1, 2, 3, 4, 5]
+#for num in numbers :
+#    print(num)
+
+x = range(6)
+for i in x :
+    print(i)
+
+x = range(1, 21)
+for i in x :
+    print(i)
+
+x = range(3, 20, 4)
+for i in x
+    print(i)
+    
